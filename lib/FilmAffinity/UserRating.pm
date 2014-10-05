bd18d1b3e315364fdf8ce10585113ea7ae09f466
William Belle <william.belle@gmail.com>
2014-10-05 17:08:22 +0200

    clean UserRating.pm

--- lib/FilmAffinity/UserRating.pm
+++ lib/FilmAffinity/UserRating.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 
 use Encode;
+use Readonly;
 use Text::Trim;
 use LWP::RobotUA;
 use HTML::TreeBuilder;
@@ -34,16 +35,16 @@ Get filmaffinity voted movies from a user
     use FilmAffinity::UserRating;
 
     my $parser = FilmAffinity::UserRating->new( userID => '123456' );
-    
+
     my $ref_movies = $parser->parse();
-    
+
 Via the command-line program filmaffinity-get-ratings.pl
-    
+
 =head1 DESCRIPTION
 
 =head2 Overview
 
-FilmAffinity::UserRating is a Perl interface to FilmAffinity. You can use 
+FilmAffinity::UserRating is a Perl interface to FilmAffinity. You can use
 this module to retrieve your rated movies.
 
 =head2 Constructor
@@ -53,9 +54,9 @@ this module to retrieve your rated movies.
 =item new()
 
 Object's constructor. You should pass as parameter the userID
- 
-    my $parser = FilmAffinity::UserRating->new( userID => '123456' ); 
-    
+
+    my $parser = FilmAffinity::UserRating->new( userID => '123456' );
+
 =back
 
 =head2 Options
@@ -66,13 +67,13 @@ Object's constructor. You should pass as parameter the userID
 
 Set the minimum delay between requests to the server, in seconds.
 
-    my $parser = FilmAffinity::UserRating->new( 
+    my $parser = FilmAffinity::UserRating->new(
       userID => '123456',
       delay  => 20,
     );
-    
-By default, the delay is 5 seconds 
-    
+
+By default, the delay is 5 seconds
+
 =back
 
 =head1 ACCESSORS
@@ -83,9 +84,9 @@ get userID
 
 =cut
 
-has userID => ( 
-  is       => 'ro', 
-  isa      => 'Int', 
+has userID => (
+  is       => 'ro',
+  isa      => 'Int',
   required => 1,
 );
 
@@ -95,30 +96,31 @@ get username
 
 =cut
 
-has username => ( 
-  is     => 'ro', 
+has username => (
+  is     => 'ro',
   isa    => 'Str',
-  writer => 'p_username', 
+  writer => 'p_username',
 );
 
 =head2 $parser->movies
 
 get movies
 
-=cut 
+=cut
 
-has movies => ( 
-  is      => 'rw', 
-  isa     => 'HashRef[Str]', 
-  default => sub { {} }, 
-); 
+has movies => (
+  is      => 'rw',
+  isa     => 'HashRef[Str]',
+  default => sub { {} },
+);
 
-has ua => ( 
+has ua => (
   is      => 'rw',
   isa     => 'LWP::RobotUA',
   traits  => [qw/Private/],
-); 
- 
+);
+
+Readonly my $SECONDS => 5;
 my $RATING_URL   = 'http://www.filmaffinity.com/en/userratings.php?orderby=2&';
 my $XPATH_ID     = '//table[@class="amovie_info"]/tr/child::*[1]/div/@data-movie-id';
 my $XPATH_TITLE  = '//div[@class="mc-title"]/a';
@@ -127,11 +129,13 @@ my $XPATH_RATING = '//table[@class="amovie_info"]/tr/child::*[2]/div/child::*[1]
 sub BUILD {
   my ($self, $args) = @_;
 
-  $self->ua( buildRobot( $args->{delay} || 5 ) );
-} 
-   
-=head1 METHODS   
-      
+  $self->ua( buildRobot( $args->{delay} || $SECONDS ) );
+
+  return;
+}
+
+=head1 SUBROUTINES/METHODS
+
 =head2 $parser->parse()
 
 This function parses all rating pages of filmaffinity from a user.
@@ -139,7 +143,7 @@ This function parses all rating pages of filmaffinity from a user.
     my $ref_movies = $parser->parse();
 
 =cut
-  
+
 sub parse {
   my $self = shift;
 
@@ -161,63 +165,64 @@ sub parse {
 
 =head2 $parser->parseString($content)
 
-This function parses a page of filmaffinity that is available as 
-a single string in memory. 
+This function parses a page of filmaffinity that is available as
+a single string in memory.
 
     $parser->parseString($content);
 
 =cut
-  
+
 sub parseString {
   my ($self, $content) = @_;
-  
+
   $content = demoronize($content);
   $content = decode('iso8859-1', $content);
- 
+
   my $tree = HTML::TreeBuilder->new();
-  $tree->parse($content);    
+  $tree->parse($content);
   $self->p_username($tree->findvalue( '//span[@id="nick"]' ));
-     
+
   my @ids       = $tree->findnodes_as_strings( $XPATH_ID );
   my @titles    = $tree->findnodes_as_strings( $XPATH_TITLE );
   my @ratings   = $tree->findnodes_as_strings( $XPATH_RATING );
-  
+
   $self->p_buildMovieInfo(\@ids, \@titles, \@ratings);
 
   $tree->delete();
+  return;
 }
 
 private_method p_buildUrl => sub {
   my ($self, $page) = @_;
-    
+
   return $RATING_URL.'p='.$page.'&user_id='.$self->userID;
 };
- 
+
 private_method p_buildMovieInfo => sub {
   my ($self, $ref_ids, $ref_titles, $ref_ratings) = @_;
-  
-  for my $i (0..(@{$ref_titles}-1)){ 
+
+  for my $i (0..(@{$ref_titles}-1)){
     $self->movies->{${$ref_ids}[$i]} = {
       'title'  => demoronize(${$ref_titles}[$i]),
-      'rating' => ${$ref_ratings}[$i],  
+      'rating' => ${$ref_ratings}[$i],
     }
-  }    
+  }
 };
 
 private_method p_isNextPage => sub {
-  my ($self, $content) = @_; 
-  
-  if ($content =~ m/>&gt;&gt;<\/a><\/div><\/div><\/td>/){
+  my ($self, $content) = @_;
+
+  if ($content =~ m/>&gt;&gt;<\/a><\/div><\/div><\/td>/xms){
     return 1;
   }
-  return 0; 
+  return 0;
 };
 
 =head1 AUTHOR
 
 William Belle, C<< <william.belle at gmail.com> >>
 
-=head1 BUGS
+=head1 BUGS AND LIMITATIONS
 
 Please report any bugs or feature requests to C<bug-filmaffinity-userrating at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=FilmAffinity-UserRating>.  I will be notified, and then you'll
